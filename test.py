c25d5c84ea9f0e131b66ad5eb5bc8eaa4e5d4d92
Bradley <bradley@breadnet.co.uk>
2022-03-21 20:48:28 +0000

    added code

---
+++ test.py
@@ -0,0 +1,20 @@
+#from urllib.parse import urlencode, quote_plus
+#from urllib import parse
+import requests, urllib.parse
+
+
+#get user input
+badge = input("Name of badge: ")
+color_input = input("Color with the #: ")
+
+#Post processing to encode and remove spaces
+a = urllib.parse.quote(color_input)
+underscores = badge.replace(" ", "_")
+nospaces = badge.replace(" ", "")
+
+url = f'(https://img.shields.io/badge/{underscores}-{a}?style=for-the-badge&logo={nospaces}&logoColor=white)'
+front = ("| " + badge + " | ")
+mdrender = ("!" + "[" + badge + "]")
+codesnippet = ("`" + url + "`")
+#Deliver the result to terminal
+print(front + mdrender + url + "|" + codesnippet)
